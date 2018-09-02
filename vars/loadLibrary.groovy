dded1b2293a84c2e2c06cb0cbf2d00a7588a993c
ashwin <ashwinkn@hotmail.com>
2018-09-02 10:49:26 -0500

    Fixed library load

--- vars/loadLibrary.groovy
+++ vars/loadLibrary.groovy
@@ -1,7 +1,7 @@
 
 
 def call() {
-// sh('cp -r jenkins jenkins-lib; cd jenkins-lib && rm -rf .git && git init && git add --all . && git commit -m init &> /dev/null') 
+    sh('cp -r jenkins jenkins-lib; cd jenkins-lib && rm -rf .git && git init && git add --all . && git commit -m init &> /dev/null') 
     def repoPath = sh(returnStdout: true, script: 'pwd').trim() + "/jenkins"
     library identifier: 'local-lib@master', retriever: modernSCM([$class: 'GitSCMSource', remote: repoPath])
 }
