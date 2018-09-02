81121e25d33ea50fbe6d0e97395747c286f4a7c0
ashwin <ashwinkn@hotmail.com>
2018-09-02 12:14:28 -0500

    Fixed git errors

--- vars/loadLibrary.groovy
+++ vars/loadLibrary.groovy
@@ -1,7 +1,7 @@
 
 
 def call() {
-    sh('cp -r jenkins jenkins-lib; cd jenkins-lib && rm -rf .git && git init && git add --all . && git commit -m init') 
+    sh('cp -r jenkins jenkins-lib; cd jenkins-lib && rm -rf .git && git init && git add --all . && git config user.email test@test && git config user.name test && git commit -m init') 
     def repoPath = sh(returnStdout: true, script: 'pwd').trim() + "/jenkins-lib"
     sh "git ls-remote ${repoPath}"
     library identifier: 'local-lib@master', retriever: modernSCM([$class: 'GitSCMSource', remote: repoPath])
