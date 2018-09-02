e6eea43fb0d927cb226b58a94ad385badd399621
ashwin <ashwinkn@hotmail.com>
2018-09-02 11:05:30 -0500

    Debug

--- vars/loadLibrary.groovy
+++ vars/loadLibrary.groovy
@@ -3,5 +3,6 @@
 def call() {
     sh('cp -r jenkins jenkins-lib; cd jenkins-lib && rm -rf .git && git init && git add --all . && git commit -m init &> /dev/null') 
     def repoPath = sh(returnStdout: true, script: 'pwd').trim() + "/jenkins-lib"
+    sh "git ls-remote ${repoPath}"
     library identifier: 'local-lib@master', retriever: modernSCM([$class: 'GitSCMSource', remote: repoPath])
 }
